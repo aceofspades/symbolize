89460712f795d6177fed347ce31ab6efd2b72883
Doug Puchalski <doug316@gmail.com>
2014-01-05 09:44:23 -0800

    ActiveModel support

--- lib/symbolize/railtie.rb
+++ lib/symbolize/railtie.rb
@@ -4,7 +4,7 @@ module Symbolize
   class Railtie < Rails::Railtie
     initializer 'symbolize.insert_into_active_record' do
       ActiveSupport.on_load :active_record do
-        ::ActiveRecord::Base.send :include, Symbolize::ActiveRecord
+        ::ActiveRecord::Base.send :include, Symbolize::ActiveModel
       end
     end
   end
